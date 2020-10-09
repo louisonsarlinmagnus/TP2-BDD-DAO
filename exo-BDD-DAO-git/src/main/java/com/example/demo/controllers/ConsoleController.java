14c34ea16ec56425b5be4cd49c8cdec9f1a5cb00
louisonsarlinmagnus <70757160+louisonsarlinmagnus@users.noreply.github.com>
2020-10-09 10:09:42 +0200

    Binding de la /console, /persons et /person en fournissant l'id

---
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/controllers/ConsoleController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ConsoleController {
+
+	@RequestMapping(value = "/console", method = RequestMethod.GET)
+	String console() {
+		return "redirect:/h2-console";
+	}
+}
---
+++ exo-BDD-DAO-git/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dao.PersonDAO;
+import com.example.demo.entities.Person;
+
+
+
+
+@RestController
+public class MainController {
+	
+	@Autowired
+	PersonDAO personDAO;
+	
+	@RequestMapping(value = "/person", method = RequestMethod.GET)
+	Person person(@RequestParam(name = "method", required = true, defaultValue = "id") String method, @RequestParam(name = "value", required = false, defaultValue = "1") String value) {
+		
+		Person person;
+		
+		switch (method) {
+		case "person":
+			person=personDAO.findById(Long.valueOf(value)).get();
+			break;
+		default:
+			throw new RuntimeException("C'est CASSE");
+		}
+		
+		return person;
+		
+	}
+	
+	@RequestMapping(value = "/persons", method = RequestMethod.GET)
+	List<Person> persons(@RequestParam(name = "method", required = true, defaultValue = "id") String method, @RequestParam(name = "value", required = false) String value) {
+		List<Person> listPersons;
+		
+		switch (method) {
+		case "persons":
+			listPersons = personDAO.findAllPersons();
+			break;
+		default:
+			throw new RuntimeException("C'est CASSé");
+		}
+		return listPersons;
+		
+	}
+		
+	
+}
